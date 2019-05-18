24ec942d4392645e57f7d920d0320690d9394b66
Archmonger <>
2019-05-18 02:32:55 -0700

    test new promise

--- Extras/bbf_organizr_minimal_edition_tooltips.js
+++ Extras/bbf_organizr_minimal_edition_tooltips.js
@@ -2,12 +2,20 @@ var children = $(".allTabsList");
 for (var i = 0; i < children.length; i++) {
 	children[i].setAttribute("data-tippy-content", children[i].getAttribute("data-tab-name"))
 }
-$.getScript('https://unpkg.com/popper.js');
-$.getScript('https://unpkg.com/tippy.js', function() {
-	tippy(".allTabsList", {
-		placement: 'right',
-		boundary: document.getElementById('page-wrapper'),
-		touch: false,
-		theme: 'bbf'
-	})
+
+var getTippy = new Promise(function() {
+	$.getScript('https://unpkg.com/tippy.js', function() {
+		tippy(".allTabsList", {
+			placement: 'right',
+			boundary: document.getElementById('page-wrapper'),
+			touch: false,
+			theme: 'bbf'
+		})
+	});
 });
+
+var getPopper = new Promise(function() {
+	$.getScript('https://unpkg.com/popper.js');
+});
+
+getPopper.then(getTippy);
