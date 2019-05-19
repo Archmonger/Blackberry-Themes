184dfe94e6a631aa220a85352b5f0e489f67219a
Archmonger <>
2019-05-18 23:50:58 -0700

    proper load order

--- Extras/bbf_organizr_minimal_edition_tooltips.js
+++ Extras/bbf_organizr_minimal_edition_tooltips.js
@@ -3,19 +3,13 @@ for (var i = 0; i < children.length; i++) {
 	children[i].setAttribute("data-tippy-content", children[i].getAttribute("data-tab-name"))
 }
 
-var getTippy = new Promise(function() {
+$.getScript('https://unpkg.com/popper.js', function() {
 	$.getScript('https://unpkg.com/tippy.js', function() {
 		tippy(".allTabsList", {
 			placement: 'right',
 			boundary: document.getElementById('page-wrapper'),
 			touch: false,
 			theme: 'bbf'
-		})
+		});
 	});
 });
-
-var getPopper = new Promise(function() {
-	$.getScript('https://unpkg.com/popper.js');
-});
-
-getPopper.then(getTippy);
