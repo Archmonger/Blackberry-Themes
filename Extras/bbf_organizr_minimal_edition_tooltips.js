75d22c9a5e90576fc92e2f2642ccc748c0350f6e
Archmonger <>
2019-05-18 23:52:36 -0700

    include version numbers

--- Extras/bbf_organizr_minimal_edition_tooltips.js
+++ Extras/bbf_organizr_minimal_edition_tooltips.js
@@ -3,8 +3,8 @@ for (var i = 0; i < children.length; i++) {
 	children[i].setAttribute("data-tippy-content", children[i].getAttribute("data-tab-name"))
 }
 
-$.getScript('https://unpkg.com/popper.js', function() {
-	$.getScript('https://unpkg.com/tippy.js', function() {
+$.getScript('https://unpkg.com/popper.js@1', function() {
+	$.getScript('https://unpkg.com/tippy.js@4', function() {
 		tippy(".allTabsList", {
 			placement: 'right',
 			boundary: document.getElementById('page-wrapper'),
