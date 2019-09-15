5a8af6c75e322c3638560eec41b35e3767880d9a
Archmonger <>
2019-09-14 21:06:13 -0700

    Create the JS for removing topnav

---
+++ Extras/minimal_edition_remove_topnav.js
@@ -0,0 +1,3 @@
+$('head').append('<link rel="stylesheet" href="https://archmonger.github.io/Blackberry-Themes/Resources/minimal_edition_remove_topnav.css" type="text/css" />');
+$('.navbar-right').find('.dropdown:last').prependTo($('#side-menu'));
+$('a[onclick="reloadCurrentTab();"]').parent().appendTo($('#side-menu'));
