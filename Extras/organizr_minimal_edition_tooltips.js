9041f9cf3c930dbca885ec568ba809803516c665
Archmonger <>
2019-09-15 23:21:39 -0700

    add groups selector to tippy

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -1,11 +1,10 @@
-var children = $(".allTabsList");
-for (var i = 0; i < children.length; i++) {
-	children[i].setAttribute("data-tippy-content", children[i].getAttribute("data-tab-name"))
+var sidenavElements = $(".allTabsList,.allGroupsList");
+for (var i = 0; i < sidenavElements.length; i++) {
+	sidenavElements[i].setAttribute("data-tippy-content", sidenavElements[i].getAttribute("data-tab-name"))
 }
-
 $.getScript('https://unpkg.com/popper.js@1', function() {
 	$.getScript('https://unpkg.com/tippy.js@4', function() {
-		tippy(".allTabsList", {
+		tippy(sidenavElements, {
 			placement: "right",
 			boundary: "window",
 			touch: false,
