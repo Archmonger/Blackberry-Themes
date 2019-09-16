727f369ad45c376e4471d47f0045274a0ac5225f
Archmonger <>
2019-09-16 01:11:10 -0700

    temporary revert until testable

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -1,10 +1,10 @@
-var sidenavElements = $(".allTabsList,.allGroupsList");
+var sidenavElements = $(".allTabsList");
 for (var i = 0; i < sidenavElements.length; i++) {
 	sidenavElements[i].setAttribute("data-tippy-content", sidenavElements[i].getAttribute("data-tab-name"))
 }
 $.getScript('https://unpkg.com/popper.js@1', function() {
 	$.getScript('https://unpkg.com/tippy.js@4', function() {
-		tippy(".allTabsList,.allGroupsList", {
+		tippy(".allTabsList", {
 			placement: "right",
 			boundary: "window",
 			touch: false,
