accc8cdde95cbeaa177df7c449fb7079728b3747
Archmonger <>
2019-09-18 23:16:26 -0700

    Handle waiting logic internally

--- Extras/theme_installer.js
+++ Extras/theme_installer.js
@@ -1,17 +1,11 @@
-function waitToExist(selector, callback) {
-	if ($(selector).length) {
-		callback();
-	} else {
-		setTimeout(function() {
-			waitForEl(selector, callback);
-		}, 100);
-	}
-};
-
 function themeInstaller(tabName, themeUrl) {
 	var frameName = "#frame-" + tabName;
 	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />'
-	waitToExist(frameName, function() {
+	if ($(frameName).length) {
 		$(frameName).contents().find("head").append(styleSheet);
-	});
-}
+	} else {
+		setTimeout(function() {
+			themeInstaller(tabName, themeUrl);
+		}, 250);
+	}
+};
