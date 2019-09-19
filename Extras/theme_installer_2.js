c6c8fdd81dc4e420d806a900bc019497399ebc51
Archmonger <>
2019-09-19 00:05:03 -0700

    Swap 3 and 2 to be chronological

--- Extras/theme_installer_2.js
+++ Extras/theme_installer_2.js
@@ -1,29 +1,11 @@
-function elementReady(selector) {
-	return new Promise((resolve, reject) => {
-		let el = document.querySelector(selector);
-		if (el) {
-			resolve(el);
-		}
-		new MutationObserver((mutationRecords, observer) => {
-				// Query for elements matching the specified selector
-				Array.from(document.querySelectorAll(selector)).forEach((element) => {
-					resolve(element);
-					//Once we have resolved we don't need the observer anymore.
-					observer.disconnect();
-				});
-			})
-			.observe(document.documentElement, {
-				childList: true,
-				subtree: true
-			});
-	});
-}
-
-function themeInstallerr(tabName, themeUrl) {
+function themeInstaller(tabName, themeUrl) {
 	var frameName = "#frame-" + tabName;
-	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />';
-	elementReady(frameName).then(
-		(loadJS) => {
-			$(frameName).contents().find("head").append(styleSheet)
-		});
+	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />'
+	if ($(frameName).length) {
+		$(frameName).contents().find("head").append(styleSheet);
+	} else {
+		setTimeout(function() {
+			themeInstaller(tabName, themeUrl);
+		}, 250);
+	}
 };
