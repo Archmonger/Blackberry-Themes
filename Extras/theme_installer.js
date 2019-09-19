ad0b8978d416f1d7a672556e4b73a5d6b1f27a5b
Archmonger <>
2019-09-18 23:09:46 -0700

    add wait to exist logic

--- Extras/theme_installer.js
+++ Extras/theme_installer.js
@@ -1,7 +1,17 @@
+function waitToExist(selector, callback) {
+	if ($(selector).length) {
+		callback();
+	} else {
+		setTimeout(function() {
+			waitForEl(selector, callback);
+		}, 100);
+	}
+};
+
 function themeInstaller(tabName, themeUrl) {
 	var frameName = "#frame-" + tabName;
 	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />'
-	$(frameName).load(function() {
+	waitToExist(frameName, function() {
 		$(frameName).contents().find("head").append(styleSheet);
 	});
 }
