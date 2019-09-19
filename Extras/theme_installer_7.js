2d021cacd3f87cf52a95a5545d04ace91dac8514
Archmonger <>
2019-09-19 11:29:24 -0700

    seventh attempt

---
+++ Extras/theme_installer_7.js
@@ -0,0 +1,13 @@
+function themeInstaller(tabName, themeUrl) {
+	console.log('resetting for the ' + reset + ' time');
+	reset++;
+	var frameName = "#frame-" + tabName;
+	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />'
+	if ($(frameName).length) {
+		$(frameName).contents().find("head").append(styleSheet);
+	} else if (reset <= 200000) {
+		setTimeout(function() {
+			themeInstaller(tabName, themeUrl);
+		}, 250);
+	}
+}
