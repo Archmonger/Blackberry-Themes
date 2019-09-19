e86e073ad83e8201e5c45e967fa9eecec7c50521
Archmonger <>
2019-09-18 23:56:54 -0700

    create second attempt at a theme installer

---
+++ Extras/theme_installer_2.js
@@ -0,0 +1,29 @@
+function elementReady(selector) {
+	return new Promise((resolve, reject) => {
+		let el = document.querySelector(selector);
+		if (el) {
+			resolve(el);
+		}
+		new MutationObserver((mutationRecords, observer) => {
+				// Query for elements matching the specified selector
+				Array.from(document.querySelectorAll(selector)).forEach((element) => {
+					resolve(element);
+					//Once we have resolved we don't need the observer anymore.
+					observer.disconnect();
+				});
+			})
+			.observe(document.documentElement, {
+				childList: true,
+				subtree: true
+			});
+	});
+}
+
+function themeInstallerr(tabName, themeUrl) {
+	var frameName = "#frame-" + tabName;
+	var styleSheet = '<link rel="stylesheet" href="' + themeUrl + '" type="text/css" />';
+	elementReady(frameName).then(
+		(loadJS) => {
+			$(frameName).contents().find("head").append(styleSheet)
+		});
+};
