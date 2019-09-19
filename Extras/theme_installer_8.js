183e8f6c9b611801f86f0c92aebe10be127cf24b
Archmonger <>
2019-09-19 12:42:46 -0700

    add first launch check

--- Extras/theme_installer_8.js
+++ Extras/theme_installer_8.js
@@ -1,7 +1,13 @@
 // Groot JS themeInstaller
+var firstLaunch = 1;
+
 function themeInstaller(tabName, themeUrl) {
 	var frameName = "#frame-" + tabName;
-	console.log('Searching for ' + frameName + ' to apply stylesheet ' + themeUrl);
+	if (firstLaunch) {
+		console.log('Searching for ' + frameName + ' to apply stylesheet ' + themeUrl);
+		firstLaunch = 0;
+	}
+
 	if ($(frameName).contents().find("head>style").length && $(frameName).contents().find("html>body").length) {
 		console.log(frameName + ' detected. Applying theme.');
 		var stylesheet = document.createElement("link");
