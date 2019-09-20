1107be72135c69a651544ee7dd2c1252a60b825f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-09-20 13:53:47 -0700

    even more comments, and add customizable variables.

--- Extras/theme_installer.js
+++ Extras/theme_installer.js
@@ -21,9 +21,14 @@ function elementReady(selector) {
 
 var retries = 0; // A count of amount of times the script has checked if the tab is fully loaded
 var retryTime = 1; // Time in MS for how often to check if the tab is loaded.
+var retrySlowdown = 500; // When retrying after this many times, slow down how fast you will retry.
+var retrySlowdownTime = 250; // Time in MS for how often to check if a tab is loaded when under a retrySlowdown state.
+var maxRetries = 1500; // Amount of retry attempts allowed.
 
 function themeInstaller(tabName, themeUrl) {
 	var frameName = "#frame-" + tabName;
+
+	// Wait for Organizr to create the iframe
 	elementReady(frameName).then(
 		(loadJS) => {
 
@@ -32,24 +37,28 @@ function themeInstaller(tabName, themeUrl) {
 				// Make sure that the styling will apply through iframe reload
 				$(frameName).on("load", function() {
 					// Frame has been fully loaded and the theme can be applied
-					console.log(frameName + ' detected. Applying theme.');
+					console.log(frameName + " detected. Applying theme.");
 					var stylesheet = document.createElement("link");
 					stylesheet.rel = "stylesheet";
 					stylesheet.href = themeUrl;
 					$(frameName).contents().find("body").append(stylesheet);
 				})
-			} else if (retries < 1500) {
+			}
 
-				// If the tab isn't ready, wait for the tab to be loaded before attempting to apply the theme
+			// If the tab isn't ready, wait for the tab to be loaded before attempting to apply the theme
+			else if (retries < maxRetries) {
 				setTimeout(function() {
 					retries++;
 					themeInstaller(tabName, themeUrl);
 					// Slow down upon excessive retry attempts to avoid lagging the browser.
-					if (retries == 500) {
-						retryTime = 250;
+					if (retries == retrySlowdown) {
+						retryTime = retrySlowdownTime;
 					}
 				}, retryTime);
-			} else {
+			}
+
+			// Tab has failed to load itself within a reasonable amount of time.
+			else {
 				console.log("Blackberry Theme Installer has reached the maximum amount of retries for " + tabName + ". Giving up.");
 			}
 		});
