2e9c09830a658faaf913d7e0beffa868b6a7cf88
Archmonger <>
2019-09-22 00:41:54 -0700

    Add tooltips to the sidenav profile picture

--- Extras/organizr_minimal_edition_remove_topnav.js
+++ Extras/organizr_minimal_edition_remove_topnav.js
@@ -2,3 +2,18 @@
 $('head').append('<link rel="stylesheet" href="https://archmonger.github.io/Blackberry-Themes/Resources/organizr_minimal_edition_remove_topnav.css" type="text/css" />');
 $('.navbar-right').find('.dropdown:last').clone().prependTo($('#side-menu'));
 $('a[onclick="reloadCurrentTab();"]').parent().clone().prependTo($('#side-menu .dropdown-menu'));
+
+// Add tooltips to the sidenav profile picture
+$("#side-menu>.dropdown").attr("data-tippy-content", $(".dropdown>.profile-pic>b").first().text());
+$("#side-menu>.dropdown").attr("class", "dropdown sidenav-profile");
+$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
+	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
+		tippy(".sidenav-profile", {
+			placement: "right-start",
+			boundary: "window",
+			touch: false,
+			theme: "bbf",
+			offset: "10, 0"
+		});
+	});
+});
