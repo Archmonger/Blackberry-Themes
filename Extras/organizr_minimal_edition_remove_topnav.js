9c95f3a32efd5d155d1e93f3f4693b665d51e01f
Archmonger <>
2019-09-21 23:14:31 -0700

    Move refresh button to user dropdown.

--- Extras/organizr_minimal_edition_remove_topnav.js
+++ Extras/organizr_minimal_edition_remove_topnav.js
@@ -1,4 +1,4 @@
 /* Created by Blackberry Themes */
 $('head').append('<link rel="stylesheet" href="https://archmonger.github.io/Blackberry-Themes/Resources/organizr_minimal_edition_remove_topnav.css" type="text/css" />');
 $('.navbar-right').find('.dropdown:last').clone().prependTo($('#side-menu'));
-$('a[onclick="reloadCurrentTab();"]').parent().clone().appendTo($('#side-menu'));
+$('a[onclick="reloadCurrentTab();"]').parent().clone().prependTo($('#side-menu .dropdown-menu'));
