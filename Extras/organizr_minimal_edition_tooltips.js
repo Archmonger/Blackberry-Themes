0fa314aad9c8138cd3d299d9ca449cae24de8b3e
Archmonger <>
2019-09-21 23:58:03 -0700

    give tooltip to logout button

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -3,6 +3,9 @@ var sidenavElements = $(".allTabsList");
 for (var i = 0; i < sidenavElements.length; i++) {
 	sidenavElements[i].setAttribute("data-tippy-content", sidenavElements[i].getAttribute("data-tab-name"))
 }
+$("#side-menu>#sign-out").attr("data-tippy-content", "Sign Out");
+$("#side-menu>#sign-out").attr("class", "allTabsList");
+
 $.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
 	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
 		tippy(".allTabsList", {
