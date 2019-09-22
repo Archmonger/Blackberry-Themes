03ab76ce5b6c578ec9a5291d9994578698f84996
Archmonger <>
2019-09-22 02:03:08 -0700

    stop myself from spawning 3 tippy's overlayed on top of eachother

--- Extras/organizr_minimal_edition_remove_topnav.js
+++ Extras/organizr_minimal_edition_remove_topnav.js
@@ -20,12 +20,25 @@ function elementReady(selector) {
 	});
 }
 
+function summonTippy(classSelector) {
+	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
+		$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
+			tippy(classSelector, {
+				placement: "right-start",
+				boundary: "window",
+				touch: false,
+				theme: "bbf",
+				offset: "10, 0"
+			});
+		});
+	});
+};
+
 $('head').append('<link rel="stylesheet" href="https://archmonger.github.io/Blackberry-Themes/Resources/organizr_minimal_edition_remove_topnav.css" type="text/css" />');
 $('.navbar-right').find('.dropdown:last').clone().prependTo($('#side-menu'));
 $('a[onclick="reloadCurrentTab();"]').parent().clone().prependTo($('#side-menu .dropdown-menu'));
 
 // Add tooltips to the sidenav items
-//, #side-menu>.dropdown>.dropdown-menu>li>a>.fa-ticket
 $("#side-menu>.dropdown").attr("data-tippy-content", $(".dropdown>.profile-pic>b").first().text()); // Profile Button
 $("#side-menu>.dropdown").attr("class", "dropdown ME-tooltips");
 $("#side-menu>.dropdown>.dropdown-menu>li>.dropdown-toggle>.ti-reload").parent().parent().attr("class", "ME-tooltips") // Reload button
@@ -34,34 +47,28 @@ $("#side-menu>.dropdown>.dropdown-menu>li>a>.ti-settings").parent().parent().att
 $("#side-menu>.dropdown>.dropdown-menu>li>a>.ti-settings").parent().parent().attr("data-tippy-content", "Account Settings")
 $("#side-menu>.dropdown>.dropdown-menu>li>a>.ti-lock").parent().parent().attr("class", "ME-tooltips") // lock button
 $("#side-menu>.dropdown>.dropdown-menu>li>a>.ti-lock").parent().parent().attr("data-tippy-content", "Lock Screen")
-
 $("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-sign-out").parent().parent().attr("class", "ME-tooltips") // Logout button
 $("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-sign-out").parent().parent().attr("data-tippy-content", "Logout")
+summonTippy(".ME-tooltips");
 
-$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-ticket").parent().parent().attr("class", "ME-tooltips") // Invite button
-$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-ticket").parent().parent().attr("data-tippy-content", "Manage Invites")
-$("#side-menu>.dropdown>.dropdown-menu>li>a>.mdi-bug").parent().parent().attr("class", "ME-tooltips") // Debug button
-$("#side-menu>.dropdown>.dropdown-menu>li>a>.mdi-bug").parent().parent().attr("data-tippy-content", "Debug Area")
+// Wait for slow loaders separately
+elementReady("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").then(
+	(elementsAreLoaded) => {
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").parent().parent().attr("class", "ME-tooltip ME-Email") // Email button
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").parent().parent().attr("data-tippy-content", "E-Mail Center")
+		summonTippy(".ME-tooltips.ME-Email");
+	});
 
 elementReady("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").then(
 	(elementsAreLoaded) => {
-		elementReady("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").then(
-			(elementsAreLoaded) => {
-				$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").parent().parent().attr("class", "ME-tooltips") // Email button
-				$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-envelope").parent().parent().attr("data-tippy-content", "E-Mail Center")
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-ticket").parent().parent().attr("class", "ME-tooltips ME-Invite") // Invite button
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.fa-ticket").parent().parent().attr("data-tippy-content", "Manage Invites")
+		summonTippy(".ME-tooltips.ME-Invite");
+	});
 
-
-
-				$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
-					$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
-						tippy(".ME-tooltips", {
-							placement: "right-start",
-							boundary: "window",
-							touch: false,
-							theme: "bbf",
-							offset: "10, 0"
-						});
-					});
-				});
-			});
+elementReady("#side-menu>.dropdown>.dropdown-menu>li>a>.mdi-bug").then(
+	(elementsAreLoaded) => {
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.mdi-bug").parent().parent().attr("class", "ME-tooltips ME-Debug") // Debug button
+		$("#side-menu>.dropdown>.dropdown-menu>li>a>.mdi-bug").parent().parent().attr("data-tippy-content", "Debug Area")
+		summonTippy(".ME-tooltips.ME-Debug");
 	});
