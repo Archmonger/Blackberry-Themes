fccbd414eb3b5d234a028456da2677a0b6221a8e
Archmonger <>
2019-09-25 14:17:18 -0700

    fix issue with being unable to add new tabs

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -4,11 +4,10 @@ for (var i = 0; i < sidenavElements.length; i++) {
 	sidenavElements[i].setAttribute("data-tippy-content", sidenavElements[i].getAttribute("data-tab-name"))
 }
 $("#side-menu>#sign-out").attr("data-tippy-content", "Sign Out");
-$("#side-menu>#sign-out").attr("class", "allTabsList");
 
 $.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
 	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
-		tippy(".allTabsList", {
+		tippy(".allTabsList,#side-menu>#sign-out", {
 			placement: "right",
 			boundary: "window",
 			touch: false,
