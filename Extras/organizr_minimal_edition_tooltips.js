7faf312ee6d2deb2b4ed3bfcf72654e0cc488039
Archmonger <16909269+Archmonger@users.noreply.github.com>
2019-09-26 02:04:04 -0700

    Support group names

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -1,13 +1,17 @@
 /* Created by Blackberry Themes */
-var sidenavElements = $(".allTabsList");
-for (var i = 0; i < sidenavElements.length; i++) {
-	sidenavElements[i].setAttribute("data-tippy-content", sidenavElements[i].getAttribute("data-tab-name"))
+var allTabsList = $(".allTabsList");
+var allGroupsList = $(".allGroupsList");
+for (var i = 0; i < allTabsList.length; i++) {
+	allTabsList[i].setAttribute("data-tippy-content", allTabsList[i].getAttribute("data-tab-name"))
+}
+for (var i = 0; i < allGroupsList.length; i++) {
+	allGroupsList[i].setAttribute("data-tippy-content", allGroupsList[i].getAttribute("data-group-name"))
 }
 $("#side-menu>#sign-out").attr("data-tippy-content", "Sign Out");
 
 $.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
 	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
-		tippy(".allTabsList,#side-menu>#sign-out", {
+		tippy(".allTabsList,#side-menu>#sign-out,.allGroupsList", {
 			placement: "right",
 			boundary: "window",
 			touch: false,
