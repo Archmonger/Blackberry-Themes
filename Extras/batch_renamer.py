3c01802825bfef24d901ab95c954d599a520cbc1
Archmonger <>
2019-09-25 19:14:38 -0700

    Create a batch renamer to help with renaming files

---
+++ Extras/batch_renamer.py
@@ -0,0 +1,16 @@
+import os 
+  
+# Function to rename multiple files 
+def main():
+    location = input("What is the location: ")
+    findStr = input("What is the string to look for: ")
+    newStr = input("What is the string to replace with: ")
+    for filename in os.listdir(location):
+        filenameNew = filename.replace(findStr,newStr)
+        dst = location + filenameNew
+        src = location + filename
+        os.rename(src, dst)
+  
+# Driver Code 
+if(__name__ == '__main__'):       
+    main() 
