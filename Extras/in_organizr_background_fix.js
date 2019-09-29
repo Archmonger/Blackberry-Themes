b2bba24549ada4942164eb1f9b9d4be2fe6fabba
Archmonger <>
2019-09-29 00:55:36 -0700

    bg fixer fixes

--- Extras/in_organizr_background_fix.js
+++ Extras/in_organizr_background_fix.js
@@ -1,6 +1,4 @@
-/* Created by Blackberry Themes */
-// Check if window is open in an iFrame
-if (window.location !== window.parent.location) {
+if (window.location == window.parent.location) {
 	// Change BG image if outside an iFrame
-	document.getElementsByTagName("html")[0].style.background = "var(--bg-light) !important";
+	document.getElementsByTagName("html")[0].style.background = getComputedStyle(document.documentElement).getPropertyValue('--bg-light');
 }
