bc407b84c0427cc9d6f0ab13461eb7d1e89232ae
Archmonger <>
2019-10-02 06:33:55 -0700

    nope

--- Extras/theme_replacer.js
+++ Extras/theme_replacer.js
@@ -3,7 +3,7 @@ function themeReplacer(tabName, themeInstallString, themeRemoveString) {
 		// First variable: Organizr tab name (case sensitive). Second variable: CSS file link or file name.
 		$.getScript('https://archmonger.github.io/Blackberry-Themes/Extras/theme_installer.js', function() {
 			// First variable: Organizr tab name (case sensitive). Second variable: Theme link or theme name (abbreviated).
-			themeRemover(tabName, themeRemoveString);
+			themeDeleter(tabName, themeRemoveString);
 			themeInstaller(tabName, themeInstallString);
 		});
 	});
