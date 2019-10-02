69cceef7e032e2e0a4c1577feb12903de8082ca0
Archmonger <>
2019-10-02 06:27:59 -0700

    fix typo

--- Extras/theme_replacer.js
+++ Extras/theme_replacer.js
@@ -1,4 +1,4 @@
-themeReplacer(tabName, themeInstallString, themeRemoveString) {
+function themeReplacer(tabName, themeInstallString, themeRemoveString) {
 	$.getScript('https://archmonger.github.io/Blackberry-Themes/Extras/theme_remover.js', function() {
 		// First variable: Organizr tab name (case sensitive). Second variable: CSS file link or file name.
 		$.getScript('https://archmonger.github.io/Blackberry-Themes/Extras/theme_installer.js', function() {
