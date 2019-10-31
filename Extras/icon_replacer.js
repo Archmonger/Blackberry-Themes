57c9592112dd2891352b1edf3a76ff7efa5b32b2
Archmonger <>
2019-10-30 19:29:18 -0700

    case insensitivity

--- Extras/icon_replacer.js
+++ Extras/icon_replacer.js
@@ -1,6 +1,6 @@
 function iconReplacer(search, replace){
   tabIcons = $(".allTabsList>a>img,.allGroupsList>a>img");
   for (var i = 0; i < tabIcons.length; i++){
-    tabIcons[i].src = tabIcons[i].src.replace(search,replace);
+    tabIcons[i].src = tabIcons[i].src.replace(search.charAt(0).toUpperCase() + search.slice(1).toLowerCase(),replace.charAt(0).toUpperCase() + replace.slice(1).toLowerCase());
   }
 }
