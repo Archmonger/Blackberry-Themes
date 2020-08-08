e0e5b0cd690a4b1d20ac342d0f2a2f18737f2dae
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-08-08 00:52:16 -0700

    clean up and add semicolons

--- Extras/organizr_minimal_edition_tooltips.js
+++ Extras/organizr_minimal_edition_tooltips.js
@@ -4,17 +4,17 @@ var allGroupsList = $(".allGroupsList");
 
 // Provide all the things with tippy content values
 for (var i = 0; i < allTabsList.length; i++) {
-	allTabsList[i].setAttribute("data-tippy-content", allTabsList[i].getAttribute("data-tab-name"))
+	allTabsList[i].setAttribute("data-tippy-content", allTabsList[i].getAttribute("data-tab-name"));
 }
 for (var i = 0; i < allGroupsList.length; i++) {
-	allGroupsList[i].setAttribute("data-tippy-content", allGroupsList[i].getAttribute("data-group-name"))
+	allGroupsList[i].setAttribute("data-tippy-content", allGroupsList[i].getAttribute("data-group-name"));
 }
 $("#side-menu>#sign-out").attr("data-tippy-content", "Sign Out");
 
 // Helper function
 function summonTippy(classSelector, tippyValues) {
-	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function() {
-		$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function() {
+	$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/popper.js", function () {
+		$.getScript("https://archmonger.github.io/Blackberry-Themes/Resources/tippy.js", function () {
 			tippy(classSelector, tippyValues);
 		});
 	});
@@ -27,7 +27,7 @@ summonTippy(".allTabsList,#side-menu>#sign-out", {
 	touch: false,
 	theme: "bbf",
 	sticky: true
-})
+});
 summonTippy(".allGroupsList", {
 	placement: "right-start",
 	boundary: "window",
@@ -36,4 +36,4 @@ summonTippy(".allGroupsList", {
 	offset: "10, 0",
 	hideOnClick: false,
 	sticky: true
-})
+});
