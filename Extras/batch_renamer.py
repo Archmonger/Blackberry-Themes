cd573b2659a74332e912b45baad0833ca5779230
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-09-22 17:10:46 -0700

    new calibre icons close #49

--- Extras/batch_renamer.py
+++ Extras/batch_renamer.py
@@ -1,16 +1,38 @@
 import os
+from pathlib import Path
+
 
 # Function to rename multiple files
-def main():
-    location = input("Directory/Location: ")
-    findStr = input("What is the string to look for: ")
-    newStr = input("What is the string to replace with: ")
-    for filename in os.listdir(location):
-        filenameNew = filename.replace(findStr,newStr)
-        dst = location + filenameNew
-        src = location + filename
-        os.rename(src, dst)
+def replacer(find_str, new_str, location):
+    for file_name in os.listdir(location):
+        if file_name.find(find_str) != -1:
+            new_file_name = file_name.replace(find_str, new_str)
+            src = location + file_name
+            dst = location + new_file_name
+            print(src)
+            print(dst)
+            os.chdir(location)
+            os.rename(file_name, new_file_name)
+
 
 # Driver Code
-if(__name__ == '__main__'):
+def main():
+    if input("[G]eneric rename, or remove copy from [I]cons names?: ").upper() == "G":
+        location = input("Directory to look within: ")
+        find_str = input("What is the string to look for: ")
+        new_str = input("What is the string to replace with: ")
+        replacer(find_str, new_str, location)
+
+    else:
+        location = Path(__file__).parent / "../Themes/"
+        find_str = " copy.png"
+        new_str = ".png"
+        for directory in os.listdir(location):
+            icon_folder_path = location / directory / "Icons"
+            if os.path.exists(icon_folder_path):
+                replacer(find_str, new_str, str(icon_folder_path.resolve()) + "/")
+
+
+# Driver Code
+if __name__ == "__main__":
     main()
