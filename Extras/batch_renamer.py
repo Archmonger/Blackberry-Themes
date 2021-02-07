2cd65758cd595d8b83eacbe84537193008e7c08c
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-06 20:50:42 -0800

    fix batch renamer

--- Extras/batch_renamer.py
+++ Extras/batch_renamer.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join as join_path
 from pathlib import Path
 
 
@@ -8,28 +9,23 @@ def replacer(find_str, new_str, location):
         if file_name.find(find_str) != -1:
             new_file_name = file_name.replace(find_str, new_str)
             print(f"Renaming {file_name} to {new_file_name}")
-            os.chdir(location)
-            os.rename(file_name, new_file_name)
+            os.rename(
+                join_path(location, file_name), join_path(location, new_file_name)
+            )
 
 
 # Driver Code
-def main():
-    if input("[G]eneric rename, or remove copy from [I]cons names?: ").upper() == "G":
-        location = input("Directory to look within: ")
-        find_str = input("What is the string to look for: ")
-        new_str = input("What is the string to replace with: ")
-        replacer(find_str, new_str, location)
+user_input = input('[G]eneric rename, or remove "copy" from [I]cons names?: ').upper()
 
-    else:
-        location = Path(__file__).parent / "../Themes/"
-        find_str = " copy.png"
-        new_str = ".png"
-        for directory in os.listdir(location):
-            icon_folder_path = location / directory / "Icons"
-            if os.path.exists(icon_folder_path):
-                replacer(find_str, new_str, str(icon_folder_path.resolve()) + "/")
+if user_input == "G":
+    loc = input("Directory to look within: ")
+    find = input("What is the string to look for: ")
+    new = input("What is the string to replace with: ")
+    replacer(find, new, loc)
 
-
-# Driver Code
-if __name__ == "__main__":
-    main()
+elif user_input == "I":
+    loc = Path(__file__).parent / "../Themes/"
+    for directory in os.listdir(loc):
+        icon_folder_path = loc / directory / "Icons"
+        if os.path.exists(icon_folder_path):
+            replacer(" copy.png", ".png", str(icon_folder_path.resolve()) + "/")
